dfb28152f808f34ffc7a72de762ae43def9e4dbf
admin <admin@DESKTOP-KUF89T6>
2024-01-29 11:11:01 +0900

    och01_git  작업 first

---
+++ och01_git/src/main/java/och01_git/Board.java
@@ -0,0 +1,52 @@
+package och01_git;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Board
+ */
+public class Board extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Board() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String title = request.getParameter("title");
+		String writer = request.getParameter("writer");
+		String content = request.getParameter("content");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html><meta charset=\"utf-8\"><body>");
+		out.println("제목 " + title + "<br />");
+		out.println("작성자 " + writer + "<br />");
+		out.println("내용 " + content);
+		out.println("</body></html>");
+		out.close();
+
+		
+	}
+
+}
